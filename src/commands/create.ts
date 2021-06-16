940a023b1e697a8673b64a14823b06fbf2ff4dd9
NWBY <sam.newby19@gmail.com>
2021-06-16 20:37:42 +0100

    Create tasks

--- src/commands/create.ts
+++ src/commands/create.ts
@@ -1,6 +1,7 @@
 import { Command, flags } from '@oclif/command'
 import * as fs from 'fs-extra'
 import * as path from 'path'
+import axios from 'axios'
 
 export default class Create extends Command {
     static description = 'Create a new task'
@@ -18,14 +19,33 @@ export default class Create extends Command {
 
     async run() {
         const { args, flags } = this.parse(Create)
+
+        const taskordConfigRead = await fs.readJsonSync(path.join(this.config.configDir, 'config.json'), { throws: false })
+
+        this.log(`${flags.done}`)
         
         if (flags.product) {
-            const taskordConfigRead = await fs.readJsonSync(path.join(this.config.configDir, 'config.json'), { throws: false })
+            let details = taskordConfigRead.webhooks.find(e => {
+                return e.product.toLowerCase() == flags.product.toLowerCase();
+            });
 
-            let details = taskordConfigRead.webhooks.find(e => e.product == flags.product)
-            this.log(details);
+            this.createTask(details, args.task, flags.done);
+        } else {
+            let details = taskordConfigRead.webhooks.find(e => e.product == 'none');
+
+            this.createTask(details, args.task, flags.done);
         }
+    }
 
-        this.log(`hello lads from /Users/Sam/projects/taskord-cli/src/commands/create.ts`)
+    createTask(config: any, task: String, done: any) {
+        const response = axios.post(config.url, {
+            task: task,
+            done: done ? done : false
+        }).then(res => {
+            this.log('Task successfully created!')
+        })
+            .catch(err => {
+                this.log('Oh! That task wasn\'t created, you may have experienced an error!')
+            })
     }
 }
